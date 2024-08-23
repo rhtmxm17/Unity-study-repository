8a13974bf49314297353d1ae1cbfd6192b1850b2
JeeSeong <rhtmxm17@gmail.com>
2024-08-23 19:07:17 +0900

    feat/키 입력을 통한 기본 조작 구현

---
+++ Unity-study/Assets/Follower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 interval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetParent(target);
+        transform.SetLocalPositionAndRotation(interval, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
