35275d3d69dc3032e5f3879802123719f1160d3c
JeeSeong <rhtmxm17@gmail.com>
2024-09-05 22:43:38 +0900

    refactor/축 입력 회전 컴포넌트 분리 및 정리

---
+++ Unity-study/Assets/Scripts/AxisInputRotator.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class AxisInputRotator : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField]
+    private AxisInfo vertical = new AxisInfo()
+    {
+        valueRangeMin = -70f,
+        valueRangeMax = 70f,
+        wrap = false,
+        speed = 5f,
+        inputAxisName = "Mouse Y",
+        invert = true,
+    };
+
+    [SerializeField]
+    private AxisInfo horizontal = new AxisInfo()
+    {
+        valueRangeMin = -180f,
+        valueRangeMax = 180f,
+        wrap = true,
+        speed = 5f,
+        inputAxisName = "Mouse X",
+        invert = false,
+    };
+
+    // CinemachineVirtualCamera의 POV 조준모드를 참고해서 구성함
+    [System.Serializable]
+    private struct AxisInfo
+    {
+        // 매 프레임 사용되는 값
+        public float value;
+        public float inputAxisValue;
+
+        // 설정값
+        public float valueRangeMin;
+        public float valueRangeMax;
+        public bool wrap;
+        public float speed;
+
+        public string inputAxisName;
+        public bool invert;
+
+        public void UpdateValue()
+        {
+            inputAxisValue = Input.GetAxis(inputAxisName);
+            value += inputAxisValue * speed * (invert ? -1f : 1f);
+            if (false == wrap)
+                value = Mathf.Clamp(value, valueRangeMin, valueRangeMax);
+        }
+    }
+
+    private void Update()
+    {
+        horizontal.UpdateValue();
+        vertical.UpdateValue();
+
+        target.localEulerAngles = new Vector3(vertical.value, horizontal.value, 0f);
+    }
+}
