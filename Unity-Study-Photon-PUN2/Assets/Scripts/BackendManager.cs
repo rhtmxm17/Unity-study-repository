ad5b7327da7a4f7b4b889b237897516961901d37
JeeSeong <rhtmxm17@gmail.com>
2024-11-18 13:45:25 +0900

    [Add]: 프로젝트에 Firebase Realtime Database 추가

--- Unity-Study-Photon-PUN2/Assets/Scripts/BackendManager.cs
+++ Unity-Study-Photon-PUN2/Assets/Scripts/BackendManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using Firebase;
 using Firebase.Extensions;
 using Firebase.Auth;
+using Firebase.Database;
 
 public class BackendManager : MonoBehaviour
 {
     private static FirebaseApp app;
     private static FirebaseAuth auth;
+    private static FirebaseDatabase database;
 
     public static FirebaseAuth Auth => auth;
+    public static FirebaseDatabase Database => database;
 
     private void Start()
     {
@@ -28,6 +31,7 @@ public class BackendManager : MonoBehaviour
                 // where app is a Firebase.FirebaseApp property of your application class.
                 app = FirebaseApp.DefaultInstance;
                 auth = FirebaseAuth.DefaultInstance;
+                database = FirebaseDatabase.DefaultInstance;
 
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
                 Debug.Log("FirebaseApp 종속성 검사 완료");
@@ -38,6 +42,7 @@ public class BackendManager : MonoBehaviour
                 // Firebase Unity SDK is not safe to use here.
                 app = null;
                 auth = null;
+                database = null;
             }
         });
     }
